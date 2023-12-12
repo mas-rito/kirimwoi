5721a3165491b2056ca8f7d432362d1ba99ad185
Rito Ramadhan <126365429+mas-rito@users.noreply.github.com>
2023-12-12 13:51:26 +0700

    feat: add midleware

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+import withAuth from "./middleware/withAuth";
+
+export function mainMiddleware(request: NextRequest) {
+  const res = NextResponse.next();
+  return res;
+}
+
+export default withAuth(mainMiddleware, ["/upload"]);
