19b37bef5c6cffbae78efac9be13fe1c1fbfd896
Rito Ramadhan <126365429+mas-rito@users.noreply.github.com>
2023-12-31 23:31:58 +0700

    update(env): change env in Single file page

--- app/(dashboard)/files/[id]/page.tsx
+++ app/(dashboard)/files/[id]/page.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
 
 const SingleFile = async ({ params }: { params: { id: string } }) => {
   const file = await getData(
-    `${process.env.NEXT_LOCAL_DOMAIN}/api/files?id=${params.id}`
+    `${process.env.NEXT_PUBLIC_DOMAIN}/api/files?id=${params.id}`
   );
   const fileShortUrl = process.env.NEXT_PUBLIC_DOMAIN + "/f/" + file.data.id;
 
