a72984da2f7a40391c6688561fb9b028da082705
Rito Ramadhan <126365429+mas-rito@users.noreply.github.com>
2024-01-02 15:42:05 +0700

    feat(Component FileList): add animation and conditional for data

--- app/_components/FileList/index.tsx
+++ app/_components/FileList/index.tsx
@@ -17,16 +17,17 @@ const FileListComponent = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [checked, setChecked] = useState(false);
   const [selectedItems, setSelectedItems] = useState<DataItem[]>([]);
-
-  console.log(selectedItems);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const fetchData = async () => {
       try {
         const result = await getData(
           `${process.env.NEXT_PUBLIC_DOMAIN}/api/files?email=${session.data?.user?.email}`
         );
         setData(result.data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -97,7 +98,30 @@ const FileListComponent = () => {
             ) : null}
           </div>
           <div className="h-96 overflow-y-auto">
-            {data &&
+            {loading ? (
+              <div className="flex justify-center mt-4">
+                <svg
+                  className="animate-spin h-6 w-6 text-gray-800"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              </div>
+            ) : data.length > 0 ? (
               filteredData.map((item: any) => (
                 <div
                   key={item.id}
@@ -125,7 +149,10 @@ const FileListComponent = () => {
                     {item.name}
                   </Link>
                 </div>
-              ))}
+              ))
+            ) : (
+              <div className="text-center mt-4">No files found</div>
+            )}
           </div>
         </div>
       </div>
